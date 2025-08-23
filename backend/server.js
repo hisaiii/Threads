135453b1b5a9234f31d51cb7d9aa10001e511ae8
rahuls2764 <sonirahul2764@gmail.com>
2025-08-23 05:59:06 +0530

    backend server

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 
-
+ 
 app.get("/",(req,res)=>{
       res.send("this is ketan")
 })
